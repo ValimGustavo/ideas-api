375b97358b691e1000f00fc4756ab9761296cf4c
ValimGustavo <gustavo.souza.valim@gmail.com>
2020-03-25 11:04:19 -0300

    connect database

---
+++ ideas-api/src/idea/idea.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from "typeorm";
+
+@Entity("idea")
+export class IdeaEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+    
+    @CreateDateColumn()
+    created: Date;
+
+    @Column('text')
+    title: string;
+
+    @Column('text')
+    description: string;
+}
